7af9dc702046aa02f666ae1f21a41bb54da5a29d
wendwin <wendwin12@gmail.com>
2024-11-30 16:59:28 +0700

    membuat halaman splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dojo/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
---
+++ lib/splash_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Future.delayed(const Duration(seconds: 3), () {
+    //   Navigator.pushReplacement(
+    //     context,
+    //     MaterialPageRoute(builder: (context) => Login()),
+    //   );
+    // });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF141F33),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/logo-dojo.png'),
+            const SizedBox(height: 25),
+            Text('Dojo',
+                style: TextStyle(
+                    color: const Color(0xFFA3EC3D),
+                    fontSize: 40,
+                    fontFamily: GoogleFonts.bebasNeue().fontFamily)),
+          ],
+        ),
+      ),
+    );
+  }
+}
