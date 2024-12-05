0bbb0bed4cf594470c3085049ecc82c1c6f90676
wendwin <wendwin12@gmail.com>
2024-12-05 16:21:59 +0700

    membuat halaman home presensi

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -63,12 +64,192 @@ class PresensiPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Presensi',
-        style: TextStyle(fontSize: 24, color: Colors.white),
+    return Stack(children: [
+      Positioned(
+        top: 0,
+        left: 0,
+        child: Image.asset(
+          'assets/images/element-base.png',
+          fit: BoxFit.cover, // Mengisi seluruh layar dengan gambar
+          // height: MediaQuery.of(context).size.height *
+          //     0.5, // Menyesuaikan ukuran gambar (hanya setengah layar)
+        ),
       ),
-    );
+      Container(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          children: [
+            Row(children: [
+              Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(0.5),
+                  borderRadius: BorderRadius.circular(100),
+                ),
+              ),
+              const SizedBox(width: 15),
+              const Text('Alex Supriadi',
+                  style: TextStyle(color: Colors.white)),
+            ]),
+            const SizedBox(height: 40),
+            Container(
+              padding: const EdgeInsets.all(15),
+              width: double.infinity,
+              height: 110,
+              decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.3),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Column(
+                children: [
+                  Text('Presensi',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontFamily: GoogleFonts.bebasNeue().fontFamily,
+                          letterSpacing: 1.5)),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        '20 Oktober 2024',
+                        style: TextStyle(
+                            color: Color(0xFFA3EC3D),
+                            fontWeight: FontWeight.w500),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(0, 40),
+                          backgroundColor: const Color(0xFFA3EC3D),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)),
+                        ),
+                        child: const Text(
+                          'Baru',
+                          style: TextStyle(
+                              color: Colors.black, fontWeight: FontWeight.w500),
+                        ),
+                      )
+                      // Container(
+                      //   width: 70,
+                      //   height: 30,
+                      //   decoration: BoxDecoration(
+                      //     color: Color(0xFFA3EC3D),
+                      //     borderRadius: BorderRadius.circular(5),
+                      //   ),
+                      //   child: Center(
+                      //     child: Text(
+                      //       'Baru',
+                      //       style: TextStyle(
+                      //           color: Colors.white, fontWeight: FontWeight.w500),
+                      //     ),
+                      //   ),
+                      // )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 40),
+            Row(children: [
+              const Icon(Icons.more_time, color: Colors.white),
+              const SizedBox(width: 5),
+              Text(
+                'Riwayat',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    letterSpacing: 1.5,
+                    fontFamily: GoogleFonts.bebasNeue().fontFamily),
+              )
+            ]),
+            const SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.all(15),
+              width: double.infinity,
+              height: 200,
+              decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.3),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.withOpacity(0.5),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          'Tanggal',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Text(
+                          'Status',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text('20 Oktober 2024',
+                          style: TextStyle(color: Colors.white)),
+                      Text('Hadir', style: TextStyle(color: Colors.white)),
+                    ],
+                  ),
+                  const SizedBox(height: 5),
+                  Column(children: [
+                    Container(
+                        width: double.infinity,
+                        height: 2,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(0.5),
+                          borderRadius: BorderRadius.circular(10),
+                        ))
+                  ]),
+                  const SizedBox(height: 15),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text('20 Oktober 2024',
+                          style: TextStyle(color: Colors.white)),
+                      Text('Hadir', style: TextStyle(color: Colors.white)),
+                    ],
+                  ),
+                  const SizedBox(height: 5),
+                  Column(children: [
+                    Container(
+                        width: double.infinity,
+                        height: 2,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(0.5),
+                          borderRadius: BorderRadius.circular(10),
+                        ))
+                  ])
+                ],
+              ),
+            ),
+            const SizedBox(height: 30),
+            const Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Text(
+                'Lihat',
+                style: TextStyle(color: Colors.white),
+              )
+            ])
+          ],
+        ),
+      ),
+    ]);
   }
 }
 
